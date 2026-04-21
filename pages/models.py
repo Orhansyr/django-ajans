ea9445393df33bb177e6802959c1830eaed5a351
orhan <osoyer4116bursa@gmail.com>
2026-04-21 19:13:23 +0300

    ikinci

--- pages/models.py
+++ pages/models.py
@@ -5,7 +5,7 @@ from django.utils.html import format_html
 
 
 class ProductCategory(models.Model):
-    name = models.CharField('Kategori Adı', max_length=100)
+    title = models.CharField('Kategori Adı', max_length=100)
     slug = models.SlugField('slug', max_length=100, unique=True)
     image = models.ImageField('Kategori Resmi', upload_to='category_images/')
      
@@ -27,7 +27,7 @@ class ProductCategory(models.Model):
 
 
 class Product(models.Model):
-    name = models.CharField('isim', max_length=100)
+    title = models.CharField('isim', max_length=100)
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE, related_name='products')
     content = HTMLField('Açıklama')
     content2 = HTMLField('Açıklama-2')
@@ -48,6 +48,7 @@ class Product(models.Model):
 
 
 class product_image(models.Model):
+    title = models.CharField('Başlık', max_length=100)
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
     image = models.ImageField('Resim', upload_to='product_images/')
 
@@ -65,6 +66,47 @@ class product_image(models.Model):
                 '<img src="{}" width="170" height="170" />', self.image.url
             )
         return "Görsel yok"    
+
+
+# Sayfa soyut sınıfı, diğer sayfa türleri için temel oluşturacak
+class page(models.Model):
+    title = models.CharField('Başlık', max_length=100)
+    content = HTMLField('Açıklama')
+    slug = models.SlugField('slug', max_length=100, unique=True)
+    meta_title = models.CharField('Meta Title', max_length=100)
+    meta_description = models.CharField('Meta Description', max_length=255)
+    created_at = models.DateTimeField('Oluşturulma Tarihi', auto_now_add=True)
+    
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = 'Sayfa'
+        verbose_name_plural = 'Sayfalar'
+        abstract = True
+
+    
+class AboutPage(page):
+    image = models.ImageField('Resim', upload_to='page_images/')
+
+    def __str__(self):
+        return self.title
+
+    @admin.display(description="Hakkımızda Resmi")
+    def image_tag(self):  
+        if self.image:
+            return format_html(
+                '<img src="{}" width="170" height="170" />', self.image.url
+            )
+        return "Görsel yok"
+        
+
+
+
+
+
+
 
 '''
 class Hero(models.Model):
